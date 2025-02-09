cb500bafe4254e43ef325a50f746b6f7a787665b
Talgat Shashakhmetov <talgatks@gmail.com>
2025-02-08 16:15:49 -0800

    Fix: rating sort for ARG and Custom sets

--- src/upload/admin/controller/extension/module/pmp/groups/arg.php
+++ src/upload/admin/controller/extension/module/pmp/groups/arg.php
@@ -26,7 +26,7 @@ class ControllerExtensionModulePMPGroupsARG extends Controller {
 			'pd.name' => $this->language->get('text_sort_name'),
 			'p.price' => $this->language->get('text_sort_price'),
 			'p.quantity' => $this->language->get('text_sort_quantity'),
-			'p.rating' => $this->language->get('text_sort_rating'),
+			'rating' => $this->language->get('text_sort_rating'),
 			'p.date_added' => $this->language->get('text_sort_added')
 		];
 
