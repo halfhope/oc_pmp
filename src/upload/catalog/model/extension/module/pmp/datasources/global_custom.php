235591ccf171fed64518f9537c0a8d30a2ca5da2
Talgat Shashakhmetov <talgatks@gmail.com>
2025-02-08 17:46:48 -0800

    Fix: empty manually selected products

--- src/upload/catalog/model/extension/module/pmp/datasources/global_custom.php
+++ src/upload/catalog/model/extension/module/pmp/datasources/global_custom.php
@@ -7,7 +7,11 @@ class ModelExtensionModulePMPDataSourcesGlobalCustom extends Model {
 	
 	public function getData($setting) {
 		$product_data = [];
-
+		
+		if (!isset($setting['products']) || empty($setting['products'])) {
+			return $product_data;
+		}
+		
 		$setting['product_ids'] = array_keys($setting['products']);
 
 		list($fields, $join, $where, $sort_order, $limit) = $this->buildProductQuery($setting);
